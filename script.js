4c7064a77a21a0ed5e0541a94b150eb02c57971a
Lautaro Florindo <lautaroflorindo@gmail.com>
2023-12-08 10:22:12 +1100

    bye lau

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const worldCupDate = new Date("10 June 2026 23:00");
+const worldCupDate = new Date("11 December 2023 11:00");
 
 
 function countdown() {
@@ -8,17 +8,17 @@ function countdown() {
         let seconds = Math.floor(timeLeft / 1000) % 60;
         let minutes = Math.floor(timeLeft / 1000 / 60) % 60;
         let hours = Math.floor(timeLeft / 1000 / 60 / 60) % 24;
-        let days = Math.floor(timeLeft / 1000 / 60 / 60 / 24) % 30;
+        let days = Math.floor((timeLeft / 1000 / 60 / 60 / 24) % 30) * 24;
         let months = Math.floor(timeLeft / 1000 / 60 / 60 / 24 / 30) % 12;
         let years = Math.floor((timeLeft / 1000 / 60 / 60 / 24 / 30 / 12))
 
 
         document.getElementById("seconds").innerHTML = seconds;
         document.getElementById("mins").innerHTML = minutes;
-        document.getElementById("hours").innerHTML = hours;
+        // document.getElementById("hours").innerHTML = hours;
         document.getElementById("days").innerHTML = days;
-        document.getElementById("months").innerHTML = months;
-        document.getElementById("years").innerHTML = years;
+        // document.getElementById("months").innerHTML = months;
+        // document.getElementById("years").innerHTML = years;
     } else{
         document.getElementById("countdown-container").innerHTML = `<p class="final-text"> THE TIME HAS COME </p>`
     }
