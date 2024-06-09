6e9223a4677e84668b94bbf3ec3255c63874a0d3
Lautaro Florindo <lautaroflorindo@gmail.com>
2024-06-09 21:46:22 +0200

    New Year's Eve COUNTDOWN

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const worldCupDate = new Date("11 December 2023 12:35");
+const worldCupDate = new Date("31 December 2024 23:59");
 
 
 function countdown() {
@@ -8,19 +8,19 @@ function countdown() {
         let seconds = Math.floor(timeLeft / 1000) % 60;
         let minutes = Math.floor(timeLeft / 1000 / 60) % 60;
         let hours = Math.floor(timeLeft / 1000 / 60 / 60) % 24;
-        let days = Math.floor((timeLeft / 1000 / 60 / 60));
+        let days = Math.floor(timeLeft / 1000 / 60 / 60 / 24) % 30;
         let months = Math.floor(timeLeft / 1000 / 60 / 60 / 24 / 30) % 12;
-        let years = Math.floor((timeLeft / 1000 / 60 / 60 / 24 / 30 / 12))
+        // let years = Math.floor((timeLeft / 1000 / 60 / 60 / 24 / 30 / 12))
 
 
         document.getElementById("seconds").innerHTML = seconds;
         document.getElementById("mins").innerHTML = minutes;
-        // document.getElementById("hours").innerHTML = hours;
+        document.getElementById("hours").innerHTML = hours;
         document.getElementById("days").innerHTML = days;
-        // document.getElementById("months").innerHTML = months;
+        document.getElementById("months").innerHTML = months;
         // document.getElementById("years").innerHTML = years;
     } else{
-        document.getElementById("countdown-container").innerHTML = `<p class="final-text">LOVE YOU BITCHES</p>`
+        document.getElementById("countdown-container").innerHTML = `<p class="final-text">HAPPY NEW YEAR</p>`
     }
 }
 
